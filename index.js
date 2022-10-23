1f5dfd57e0f56d4dda5361dcce83f86446f138ac
Lorenzo Zubani <lorenzozubani1999@gmail.com>
2022-10-23 15:52:28 +0200

    aggiunti initalLocation , commandsRiver (da rivere), elaborateCommands; aggiunto l'edge al planet

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let rover = {
   location: location,
 };
 
+let description;
+
 const obstacles = {
   F: {
     obstacle: true,
@@ -34,38 +36,114 @@ const planet = [
   ["X", "X", "X", "O"],
 ];
 
-const roverLog = (location) => {};
+const initalLocation = () => {
+  location = {
+    x: 0,
+    y: 0,
+  };
+};
+
+const commands = ["S", "W", "W", "S"];
+
+const commandsRover = () => {
+  for (let i = 0; i < 4; i++) {
+    let command;
+    console.log(`Give commands to the Rover:`);
+    //insert from input
+    commands.push(command);
+  }
+};
+
+const elaborateCommans = (commands) => {
+  for (let i = 0; i < commands.length; i++) {
+    let command = commands[i];
+    moveRover(command);
+  }
+};
+
+let keys = Object.keys(obstacles);
+const foundObstacle = (currentLocation) => {
+  for (keys in obstacles) {
+    if (currentLocation == keys) {
+      description = obstacles[keys].description;
+    }
+  }
+  console.log("Obstacle: " + description);
+  console.log(
+    "Aborted the mission! reported the obstacle! \n The river star at new position: "
+  );
+  initalLocation();
+};
+
+let y;
+let x;
+let currentLocation;
 
 const moveRover = (direction) => {
-  let currentLocation;
   switch (direction) {
     case "N":
-      location.y++;
+      y = location.y;
+      y++;
+      location.y = y;
+      if (location.y < 0) {
+        location.y = 0;
+      }
+      if (location.y > 3) {
+        location.y = 0;
+      }
       currentLocation = planet[location.x][location.y];
       if (currentLocation != "X") {
         foundObstacle(currentLocation);
       }
+      console.log("X:" + location.x + " Y:" + location.y);
       break;
     case "S":
-      location.y--;
+      y = location.y;
+      y--;
+      location.y = y;
       if (location.y < 0) {
+        location.y = planet.length - 1;
+      }
+      if (location.y > 3) {
         location.y = 0;
       }
       currentLocation = planet[location.x][location.y];
-      console.log(currentLocation);
+      if (currentLocation != "X") {
+        foundObstacle(currentLocation);
+      }
+      console.log("X:" + location.x + " Y:" + location.y);
       break;
     case "E":
-      location.x++;
-      currentLocation = planet[location.x][location.y];
-      console.log(currentLocation);
-      break;
-    case "W":
-      location.x--;
+      x = location.x;
+      x++;
+      location.x = x;
       if (location.x < 0) {
-        location.x = 0;
+        location.y = 0;
+      }
+      if (location.x > 3) {
+        location.y = 0;
       }
       currentLocation = planet[location.x][location.y];
-      console.log(currentLocation);
+      if (currentLocation != "X") {
+        foundObstacle(currentLocation);
+      }
+      console.log("X:" + location.x + " Y:" + location.y);
+      break;
+    case "W":
+      x = location.x;
+      x--;
+      location.x = x;
+      if (location.x < 0) {
+        location.x = planet.length - 1;
+      }
+      if (location.x > 3) {
+        location.y = 0;
+      }
+      currentLocation = planet[location.x][location.y];
+      if (currentLocation != "X") {
+        foundObstacle(currentLocation);
+      }
+      console.log("X:" + location.x + " Y:" + location.y);
       break;
     default:
       console.log(
@@ -74,19 +152,10 @@ const moveRover = (direction) => {
       break;
   }
 };
+console.log(planet.length);
 
-moveRover(rover.direction);
-
-let keys = Object.keys(obstacles);
-
-const foundObstacle = (currentLocation) => {
-  for (keys in obstacles) {
-    if (currentLocation == keys) {
-      description = obstacles[keys].description;
-    }
-  }
-  console.log("Obstacle: " + description);
-};
-
+console.log(
+  `Your Rover is operative and positioned at X:${location.x} Y:${location.y}`
+);
+elaborateCommans(commands);
 /* console.log("Your location is X: " + location.x + " Y: " + location.y); */
-foundObstacle("O");
